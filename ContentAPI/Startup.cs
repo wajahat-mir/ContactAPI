31a61a15ee75eb3427dbda6d5b5fd6bebb47fafb
Wajahat Mir <WMir@dacgroup.com>
2021-09-12 15:03:23 -0400

    * include base Service, Repository

--- ContentAPI/Startup.cs
+++ ContentAPI/Startup.cs
@@ -1,3 +1,4 @@
+using ContentAPI.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,7 +27,7 @@ namespace ContentAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDependencyInjections();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
